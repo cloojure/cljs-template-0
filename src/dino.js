3157747b25b643841a894ef8f99fd4d5584e1973
Alan Thompson <cloojure@gmail.com>
2018-04-04 21:44:18 -0700

    Works for constructor fn `makeFriendly`

--- src/dino.js
+++ src/dino.js
@@ -2,3 +2,9 @@
 // dno.globalObject = { a: 1, b: 2, c: 3 };
 
 globalObject = { a: 1, b: 2, c: 3 };
+
+makeFriendly = function() {
+    var friendly = {};
+    friendly.message = "Howdy!";
+    return friendly;
+}
---
+++ src/fred/AppMeasurement-externs.js
@@ -0,0 +1,1 @@
+var AppMeasurement = function() {}
