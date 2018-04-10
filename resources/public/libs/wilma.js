519fa50ed90234cc32b88ab891f653cfee577a16
Alan Thompson <cloojure@gmail.com>
2018-04-10 07:55:11 -0700

    misc

--- resources/public/libs/wilma.js
+++ resources/public/libs/wilma.js
@@ -1,10 +1,10 @@
 
-goog.provide('wilmaphony');  // 'wilmaphony' is a "virtual-namespace" that doesn't really exist
+goog.provide('wilmaPhony');  // 'wilmaPhony' is a "virtual-namespace" that doesn't really exist
 
-wilmaphony.stats  = {  lipstick : "red",  height : 5.5 };  // both are the same
-wilmaphony.stats2 = { "lipstick": "red", "height": 5.5 };
+wilmaPhony.stats  = {  lipstick : "red",  height : 5.5 };  // both are the same
+wilmaPhony.stats2 = { "lipstick": "red", "height": 5.5 };
 
-wilmaphony.makeWilma = function () {
+wilmaPhony.makeWilma = function () {
   var wilma = {};
   wilma.desc = "patient housewife";
   wilma.says = function(name) {
--- src/tst/fred/wilma.cljs
+++ src/tst/fred/wilma.cljs
@@ -1,18 +1,18 @@
 (ns tst.fred.wilma
   (:require
     [cljs.test :refer-macros [deftest is async]]
-    [wilmaphony]
+    [wilmaPhony]
   ))
 
 (deftest succeeder (is (= 3 (+ 2 1))))
 
 (deftest t-wilma
-  (println "globalObject:  " wilmaphony/stats)
-  (is (not= wilmaphony/stats
-        wilmaphony/stats2)) ; JS objs are not=
-  (is (= (js->clj wilmaphony/stats) ; must convert to clojure maps for value equality to work
-         (js->clj wilmaphony/stats2)))
-  (let [wilma     (wilmaphony/makeWilma) ]
+  (println "globalObject:  " wilmaPhony/stats)
+  (is (not= wilmaPhony/stats
+        wilmaPhony/stats2)) ; JS objs are not=
+  (is (= (js->clj wilmaPhony/stats) ; must convert to clojure maps for value equality to work
+         (js->clj wilmaPhony/stats2)))
+  (let [wilma     (wilmaPhony/makeWilma) ]
     (println "wilma =>" wilma)
     (is (= (.-desc wilma) "patient housewife"))
     (is (= (.says wilma "Fred") "Hello, Fred"))))
