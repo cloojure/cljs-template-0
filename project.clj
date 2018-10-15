78b0b5d4d21838e716b348a01a1c9e66a23e9225
Alan Thompson <cloojure@gmail.com>
2018-10-14 17:22:18 -0700

    misc

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
 (defproject flintstones "0.1.0-SNAPSHOT"
   :min-lein-version "2.7.1"
   :dependencies [[binaryage/oops "0.6.2"]
-                 [org.clojure/clojure "1.9.0"]
+                 [org.clojure/clojure "1.10.0-beta1"]
                  [org.clojure/clojurescript "1.10.339"]
                  [reagent "0.8.1"]
                  [reagent-utils "0.3.1"]
-                 [tupelo "0.9.90"]]
+                 [tupelo "0.9.105"]]
   :plugins [[lein-cljsbuild "1.1.7" :exclusions [[org.clojure/clojure]]]
             [lein-figwheel "0.5.16"]
             [lein-doo "0.1.10"]]
